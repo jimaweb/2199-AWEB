696fda4a4c46782d78feac0fd4f74b06e5baffb9
Jimwel Valdez <175762163+jimwdlab@users.noreply.github.com>
2024-12-04 11:55:16 +0800

    Initial progress

--- jimtech-master/scripts/script.js
+++
@@ -1,8 +0,0 @@
-function hamburger() {
-    var menu = document.getElementById("mobile-nav");
-    if (menu.style.display === "block") {
-    menu.style.display = "none";
-    } else {
-    menu.style.display = "block";
-    }
-}
